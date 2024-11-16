a7f6b5a4c05200a61f4765286043aadf4fb7499f
Park JeaHyun <jea5158@gmail.com>
2024-11-16 16:45:05 +0900

    답변 생성 기능 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,11 +1,15 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.answer.dto.request.AnswerCreateRequestDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/answer")
@@ -16,8 +20,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{questionId}")
-    public ResponseEntity<Answer> createAnswer(@PathVariable("questionId") Integer questionId, @RequestParam(value = "content") String content) {
-        Answer answer = this.answerService.create(questionId, content);
+    public ResponseEntity<Answer> createAnswer(@PathVariable("questionId") Integer questionId,
+        @Valid @RequestBody AnswerCreateRequestDto requestDto, BindingResult bindingResult)
+        throws BadRequestException {
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(bindingResult.toString());
+        }
+        Answer answer = this.answerService.create(questionId, requestDto);
         return ResponseEntity.ok(answer);
     }
 }
--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.answer.dto.request.AnswerCreateRequestDto;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,10 @@ public class AnswerService {
     private final QuestionService questionService;
     private final AnswerRepository answerRepository;
 
-    public Answer create(Integer questionId, String content) {
+    public Answer create(Integer questionId, AnswerCreateRequestDto requestDto) {
         Question question = questionService.get(questionId);
         Answer answer = new Answer();
-        answer.setContent(content);
+        answer.setContent(requestDto.getContent());
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         return this.answerRepository.save(answer);
